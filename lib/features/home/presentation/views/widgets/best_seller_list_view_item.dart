c6af5f1f4835b64212abd392402b49eb9adb3900
midohamza22 <91686000+midoHaz@users.noreply.github.com>
2024-05-20 15:43:09 +0300

    best seller item author and price

---
+++ lib/features/home/presentation/views/widgets/best_seller_list_view_item.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/assets_manager.dart';
+import '../../../../../core/utils/styles.dart';
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 130,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5/4,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: const DecorationImage(
+                    image: AssetImage(AssetsManager.itemImage),
+                    fit: BoxFit.fill,
+                  )),
+            ),
+          ),
+          const SizedBox(
+            width: 30.0,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * .5,
+                child: Text(
+                  "Harry Parry Harry Parry Harry Parry Harry Parry Harry Parry",
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: Styles.textStyle20
+                      .copyWith(fontFamily: 'PlayfairDisplay'),
+                ),
+              ),
+              const SizedBox(height: 3.0,),
+              const Text("M.hamza",style: Styles.textStyle14,),
+              const SizedBox(height: 3.0,),
+              Row(
+                children: [
+                  Text('19.99 ',style: Styles.textStyle20.copyWith(fontWeight: FontWeight.bold),),
+                ],
+              )
+
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,9 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:kotobaty_app/core/utils/styles.dart';
 
 import '../../../../../core/utils/assets_manager.dart';
+import 'best_seller_list_view_item.dart';
 import 'custom_app_bar.dart';
 import 'featured_book_list_view.dart';
 class HomeViewBody extends StatelessWidget {
@@ -35,48 +34,6 @@ class HomeViewBody extends StatelessWidget {
   }
 }
 
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 130,
-      child: Row(
-        children: [
-        AspectRatio(
-        aspectRatio: 2.5/4,
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(8.0),
-                  image: const DecorationImage(
-                    image: AssetImage(AssetsManager.itemImage),
-                    fit: BoxFit.fill,
-                  )),
-            ),
-          ),
-          const SizedBox(
-            width: 30.0,
-          ),
-          Column(
-            children: [
-              SizedBox(
-            width:MediaQuery.of(context).size.width*.5,
-                child: const Text(
-                  "Harry Parry Harry Parry Harry Parry Harry Parry Harry Parry",
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: Styles.textStyle20,
-                ),
-              )
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
 
 
 
