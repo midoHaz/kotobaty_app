791f183f3bcd79809a2f0181505c39d30a35be92
midohamza22 <91686000+midoHaz@users.noreply.github.com>
2024-05-20 14:44:28 +0300

    complete text style file

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/cupertino.dart';
 
 abstract class Styles{
-  static const titleMedium=TextStyle(
+  static const textStyle14=TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal, //for semiBold
+  );
+  static const textStyle16=TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500, //for semiBold
+  );
+  static const textStyle18=TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w600, //for semiBold
+  );
+  static const textStyle20=TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.normal, //for semiBold
+  );
+  static const textStyle30=TextStyle(
+    fontSize: 30,
+    fontWeight: FontWeight.normal, //for semiBold
   );
 }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -16,7 +16,7 @@ class HomeViewBody extends StatelessWidget {
           CustomAppBar(),
           FeaturedBooksListView(),
           SizedBox(height: 50,),
-          Text('Best Seller',style:Styles.titleMedium,),
+          Text('Best Seller',style:Styles.textStyle18,),
           SizedBox(height: 10,),
           BestSellerListViewItem(),
         ],
