9984892153d0517a58ef0f50f9ec6f759fcf4b2a
midohamza22 <91686000+midoHaz@users.noreply.github.com>
2024-05-20 14:55:42 +0300

    best seller item text

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:kotobaty_app/core/utils/styles.dart';
+
 import '../../../../../core/utils/assets_manager.dart';
 import 'custom_app_bar.dart';
 import 'featured_book_list_view.dart';
@@ -9,15 +12,22 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 24.0),
+      padding: EdgeInsets.symmetric(horizontal: 30.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomAppBar(),
           FeaturedBooksListView(),
-          SizedBox(height: 50,),
-          Text('Best Seller',style:Styles.textStyle18,),
-          SizedBox(height: 10,),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            'Best Seller',
+            style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 20,
+          ),
           BestSellerListViewItem(),
         ],
       ),
@@ -36,16 +46,32 @@ class BestSellerListViewItem extends StatelessWidget {
         children: [
         AspectRatio(
         aspectRatio: 2.5/4,
-        child: Container(
-          decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(8.0),
-              image: const DecorationImage(
-                image: AssetImage(AssetsManager.itemImage),
-                fit: BoxFit.fill,
-              )),
-        ),
-      ),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: const DecorationImage(
+                    image: AssetImage(AssetsManager.itemImage),
+                    fit: BoxFit.fill,
+                  )),
+            ),
+          ),
+          const SizedBox(
+            width: 30.0,
+          ),
+          Column(
+            children: [
+              SizedBox(
+            width:MediaQuery.of(context).size.width*.5,
+                child: const Text(
+                  "Harry Parry Harry Parry Harry Parry Harry Parry Harry Parry",
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: Styles.textStyle20,
+                ),
+              )
+            ],
+          )
         ],
       ),
     );
