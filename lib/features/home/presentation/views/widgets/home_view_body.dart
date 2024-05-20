44054a65e7b6317c1f1578dc4505b8f84606cca0
midohamza22 <91686000+midoHaz@users.noreply.github.com>
2024-05-20 13:52:01 +0300

    add best seller list view item image

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kotobaty_app/core/utils/styles.dart';
+import '../../../../../core/utils/assets_manager.dart';
 import 'custom_app_bar.dart';
 import 'featured_book_list_view.dart';
 class HomeViewBody extends StatelessWidget {
@@ -15,11 +16,41 @@ class HomeViewBody extends StatelessWidget {
           CustomAppBar(),
           FeaturedBooksListView(),
           SizedBox(height: 50,),
-          Text('Best Seller',style:Styles.titleMedium,)
+          Text('Best Seller',style:Styles.titleMedium,),
+          SizedBox(height: 10,),
+          BestSellerListViewItem(),
         ],
       ),
     );
   }
 }
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 130,
+      child: Row(
+        children: [
+        AspectRatio(
+        aspectRatio: 2.5/4,
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(8.0),
+              image: const DecorationImage(
+                image: AssetImage(AssetsManager.itemImage),
+                fit: BoxFit.fill,
+              )),
+        ),
+      ),
+        ],
+      ),
+    );
+  }
+}
+
 
 
