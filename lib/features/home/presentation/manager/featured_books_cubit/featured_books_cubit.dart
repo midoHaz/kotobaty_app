fcd80987e1b88acef970e3e57978c424119f0e91
midohamza22 <91686000+midoHaz@users.noreply.github.com>
2024-07-08 14:09:35 +0300

    create featured_books_cubit method

--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:kotobaty_app/features/home/data/repos/home_repo.dart';
 import 'package:meta/meta.dart';
 
 import '../../../data/models/BookModel.dart';
@@ -6,5 +7,13 @@ import '../../../data/models/BookModel.dart';
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepository) : super(FeaturedBooksInitial());
+  final HomeRepository homeRepository;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepository.fetchFeaturedBooks();
+    result.fold((failure) => emit(FeaturedBooksFailure(failure.errorMessage)),
+        (books) => emit(FeaturedBooksSuccess(books)));
+  }
 }
--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_state.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_state.dart
@@ -5,15 +5,16 @@ abstract class FeaturedBooksState {}
 
 class FeaturedBooksInitial extends FeaturedBooksState {}
 
-class FeaturedBooksLoading extends FeaturedBooksState{}
+class FeaturedBooksLoading extends FeaturedBooksState {}
 
-class FeaturedBooksFailure extends FeaturedBooksState{
+class FeaturedBooksFailure extends FeaturedBooksState {
   final String errorMessage;
+
   FeaturedBooksFailure(this.errorMessage);
 }
 
-class FeaturedBooksSuccess extends FeaturedBooksState{
+class FeaturedBooksSuccess extends FeaturedBooksState {
   final List<BookModel> books;
+
   FeaturedBooksSuccess(this.books);
 }
-
